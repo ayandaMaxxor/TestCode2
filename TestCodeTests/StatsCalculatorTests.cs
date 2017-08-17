1482241e8d6b01c14c0823193f639be809e74589
aps <ayandapatrick@gmail.com>
2017-08-17 14:55:59 +0200

    Initial Commit

---
+++ TestCodeTests/StatsCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCode;
+
+namespace TestCodeTests
+{
+    public class StatsCalculatorTests
+    {
+
+        public void DummyTestThatWillNotRunAsWeHaveNoTestFrameworkHookedUp()
+        {
+            //var sut = new StatsCalculator(SampleData.Teams, new StatsWeightingStub());
+            //var result = sut.PlayerByPlayerNumber(5);
+            // Do assertions that result is null here i.e. result.ShouldBeNull() or Assert.IsNull(result). Choose what ever method your preferred test framework uses
+        }
+    }
+}
